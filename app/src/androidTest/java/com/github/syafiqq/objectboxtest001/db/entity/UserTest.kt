44b5bb9ce454a3550abdbc002b520f4031b49495
Syafiqq <syafiq.rezpector@gmail.com>
2019-09-05 19:27:28 +0700

    add user test

--- app/src/androidTest/java/com/github/syafiqq/objectboxtest001/db/entity/UserTest.kt
+++ app/src/androidTest/java/com/github/syafiqq/objectboxtest001/db/entity/UserTest.kt
@@ -7,11 +7,13 @@ import com.github.syafiqq.objectboxtest001.MyObjectBox
 import com.github.syafiqq.objectboxtest001.entity.User
 import io.objectbox.Box
 import io.objectbox.BoxStore
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -46,5 +48,104 @@ class UserTest {
     fun tearDown() {
         userDao?.removeAll()
         db?.close()
+    }
+
+
+    @Test
+    fun it_should_select_all_entities() {
+        val entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(0)))
+    }
+
+    @Test
+    fun it_should_insert_user_with_null() {
+        userEntity?.let {
+            it.id = null
+            userDao?.put(it)
+        }
+
+        val entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(1)))
+        entities?.forEach {
+            assertThat(it?.id, `is`(CoreMatchers.equalTo(userEntity?.id)))
+            assertThat(it?.id, `is`(CoreMatchers.equalTo(1L)))
+            assertThat(it?.name, `is`(CoreMatchers.equalTo(userEntity?.name)))
+            assertThat(it?.status, `is`(CoreMatchers.equalTo(userEntity?.status)))
+        }
+    }
+
+    @Test
+    fun it_should_insert_user_with_zero() {
+        userEntity?.let {
+            it.id = 0L
+            userDao?.put(it)
+        }
+
+        val entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(1)))
+        entities?.forEach {
+            assertThat(it?.id, `is`(CoreMatchers.equalTo(userEntity?.id)))
+            assertThat(it?.id, `is`(CoreMatchers.equalTo(1L)))
+            assertThat(it?.name, `is`(CoreMatchers.equalTo(userEntity?.name)))
+            assertThat(it?.status, `is`(CoreMatchers.equalTo(userEntity?.status)))
+        }
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun it_should_not_insert_user_with_greater_than_internal_sequence() {
+        userEntity?.let {
+            it.id = 10L
+            userDao?.put(it)
+        }
+    }
+
+    @Test
+    fun it_should_delete_user() {
+        userEntity?.let { userDao?.put(it) }
+
+        var entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(1)))
+
+        entities?.forEach {
+            assertThat(it?.id, `is`(CoreMatchers.equalTo(1L)))
+        }
+
+        userEntity?.let { userDao?.remove(it) }
+
+        entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(0)))
+    }
+
+    @Test
+    fun it_should_update_name() {
+        val newText = "New Text"
+        assertThat(newText, `is`(CoreMatchers.not(CoreMatchers.equalTo(userEntity?.name))))
+
+        userEntity?.let { userDao?.put(it) }
+
+        var entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(1)))
+        assertThat(entities?.first()?.name, `is`(CoreMatchers.equalTo(userEntity?.name)))
+
+        userEntity?.name = newText
+        userEntity?.let { userDao?.put(it) }
+
+        entities = userDao?.all
+
+        assertThat(entities, `is`(CoreMatchers.instanceOf(MutableList::class.java)))
+        assertThat(entities?.size, `is`(CoreMatchers.equalTo(1)))
+        assertThat(entities?.first()?.name, `is`(CoreMatchers.equalTo(newText)))
     }
 }
