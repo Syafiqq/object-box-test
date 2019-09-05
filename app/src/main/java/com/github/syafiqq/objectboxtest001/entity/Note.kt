6e3663beb6f8f8824070f2b49d1dc24006505679
Syafiq <syafiq.rezpector@gmail.com>
2019-09-05 22:06:29 +0700

    add to one relation

--- app/src/main/java/com/github/syafiqq/objectboxtest001/entity/Note.kt
+++ app/src/main/java/com/github/syafiqq/objectboxtest001/entity/Note.kt
@@ -2,6 +2,7 @@ package com.github.syafiqq.objectboxtest001.entity
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.relation.ToOne
 import java.util.*
 
 @Entity
@@ -9,4 +10,6 @@ data class Note(
     @Id var id: Long? = null,
     var text: String? = null,
     var date: Date? = null
-)
+) {
+    lateinit var user: ToOne<User>
+}
